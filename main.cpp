d95e0cc1b78c82463cd12a9e033df6f0e2f3bb32
Fauz67 <105625958+Fauz67@users.noreply.github.com>
2023-10-29 22:35:09 -0600

    Codigo para sustituir x's en una cadena string

--- main.cpp
+++ main.cpp
@@ -1,95 +1,39 @@
 #include <iostream>
-#include <conio.h>
-#include <string.h>
+#include <algorithm>
 #include <string>
-#include <math.h>
-//#include <stdlib.h>  //funcion atoi() y atof()
-//#include <cstdlib>   para separar numeros de texto
+#include <vector>
 
 using namespace std;
 
+bool IsX (string i) { return (i=="x"); }
+
 int main()
 {
+    string s = "3+6x/3-sin(x)/4x";
+    string limP;
+    //std::replace( s.begin(), s.end(), "x", limP);
+    cout <<"Cadena dada: "<<s<< endl;
+    cout<<"Limite inferior: ";cin>>limP;
 
-    string cad;
-    //string cad ("integral(x,12,1,2)");  // (x,y,t,z)
-    cout << "Introduce tu integral: " << "\n" <<endl;
-    cin>>cad;
-
-    ///////////////////////////////////////////////////////// codigo que ayuda a obtener (x,y,t,z)
-
-    int pos1 = cad.find("(");
-    int pos2 = cad.rfind(")");
-
-    cout << "\n" << "La longitud de la cadena es: " << cad.length() << "\n" << endl;
-    //cout << pos1 << endl;
-    //cout << pos2 << "\n"<< endl;
-
-    ///////////////////////////////////////////////////////// codigo que da -cad- por partes
-
-    string cad2 = cad.substr(pos1+1,((pos2-2)-pos1+1));             //x,yum,t,z
-    string cad3 = cad2.substr(cad2.find(",")+1,cad2.length());      //yum,t,z
-    string cad4 = cad3.substr(cad3.find(",")+1,cad3.length());      //t,z
-    string cad5 = cad4.substr(cad4.find(",")+1,cad4.length());      //z
-
-    cout << cad2 << endl;
-    cout << cad3 << endl;
-    cout << cad4 << endl;
-    cout << cad5 << endl;
-
-    ///////////////////////////////////////////////////////// espacio de 1 renglon(es)
-
+    int elementos = s.length();
     cout<<"\n"<<endl;
 
-    ///////////////////////////////////////////////////////// codigo que da x,y,t,z
+    vector<string> v (elementos);
 
-    string arg1 = cad2.substr(0,cad2.find(","));
+    for (int i = 0; i < elementos;i++) {
+        v[i] = s.substr(i,1);
+    }
 
-    string arg2 = cad3.substr(0,cad3.find(","));
+    replace_if(v.begin(), v.end(), IsX, "("+limP+")");
 
-    string arg3 = cad4.substr(0,cad4.find(","));
+    cout<<"Sustituci\xA2n de la x: "<<endl;
 
-    string arg4 = cad5.substr(0,cad5.length());
+    for (int j = 0; j < v.size();j++) {
+        cout<<v[j];
+    }
 
-    cout << "Funcion:\t\t"<< arg1 << endl;
-    cout << "Limite inferior:\t"<< arg2 << endl;
-    cout << "Limite superior:\t"<< arg3 << endl;
-    cout << "Delta de x:\t\t"<< arg4 << endl;
-
-    ///////////////////////////////////////////////////////// de string a float de los argumentos y,t,z
-
-    float numeroArg2 = stof(arg2);
-    float numeroArg3 = stof(arg3);
-    float numeroArg4 = stof(arg4);
-
-    /////////////////////////////////////////////////////////
-
-    cout << "\n" << "Veces a ejecutar la suma del resultado de la funcion:\t" << (numeroArg3-numeroArg2)/numeroArg4 << " veces" << endl;
-
-    //integral(x,0,50,0.1)
+    cout<<"\n";
 
 
-    //////////////////////////////// funcion prueba
-
-    // arg1 es una cadena tipo string
-    // hay que analizar la cadena
-
-    /*Funciones a ocupar
-
-    sen(seno)
-
-    ////////////////// funcion para seno
-
-
-
-
-
-    //////////////////////////////// switch de prueba
-
-    /*switch (arg1) {
-        case
-    }*/
-
-    getch();
     return 0;
 }
